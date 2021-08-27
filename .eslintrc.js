8c6437592547617a7ccf9a796d51780f5edc615b
Jason Athanasoglou <jathanasoglou@outlook.com>
2021-08-27 15:44:54 +0300

    Eslint ignores vars which start with underscore

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': ['off'],
         '@typescript-eslint/no-unused-vars': [
             'warn',
-            { argsIgnorePattern: '^_' },
+            { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
         ],
         semi: ['error', 'always'],
     },
