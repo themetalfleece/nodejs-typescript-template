b323f0699b0aba4c6b6442d122f83481f3580f5b
Jason Athanasoglou <jathanasoglou@outlook.com>
2021-12-08 00:10:38 +0200

    fix parserOptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,14 @@ module.exports = {
     plugins: ['prettier', '@typescript-eslint'],
     parserOptions: {
         project: './tsconfig.json',
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
     extends: [
         'plugin:@typescript-eslint/recommended',
         'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
     ],
-    parserOptions: {
-        ecmaVersion: 2018,
-        sourceType: 'module',
-    },
     rules: {
         '@typescript-eslint/array-type': ['error', { default: 'array-simple' }],
         '@typescript-eslint/no-explicit-any': ['off'],
