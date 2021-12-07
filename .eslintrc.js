41dec0bb7cc21316d52896c596147c67445c1288
Jason Athanasoglou <jathanasoglou@outlook.com>
2021-12-08 00:10:56 +0200

    upgrade all packages stop extending prettier/@typescript-eslint'

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     },
     extends: [
         'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
     ],
     rules: {
