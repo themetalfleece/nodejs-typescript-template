6b1e20abc86766b8b3ce9568e9cfa072df517985
Jason Athanasoglou <jathanasoglou@outlook.com>
2022-09-18 21:43:40 +0300

    add dotenv config

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
+/**
+ * The following lines intialize dotenv,
+ * so that env vars from the .env file are present in process.env
+ */
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 export const sum = (a: number, b: number): number => {
   return a + b;
 };
